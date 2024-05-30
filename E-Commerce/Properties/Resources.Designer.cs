2816c639ef45c0b2516fbe8a7bf99f7d5aefc08a
coutnico <nicoo_rodrii@hotmail.com>
2024-05-29 23:31:50 -0300

    changue desing from nabvar and add logo user, and changues in style ViewArticulos

--- E-Commerce/Properties/Resources.Designer.cs
+++ E-Commerce/Properties/Resources.Designer.cs
@@ -109,5 +109,35 @@ namespace tp_web_equipo_19.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap LogoClosemarket {
+            get {
+                object obj = ResourceManager.GetObject("LogoClosemarket", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap NombreClosemarket {
+            get {
+                object obj = ResourceManager.GetObject("NombreClosemarket", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap username {
+            get {
+                object obj = ResourceManager.GetObject("username", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
