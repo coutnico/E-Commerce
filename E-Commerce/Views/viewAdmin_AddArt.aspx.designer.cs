7afade161e71e4e269374e768f9746700db763db
marianrp09 <marian_rp09@hotmail.com>
2024-07-04 20:34:57 -0300

    Begin chat

--- E-Commerce/Views/viewAdmin_AddArt.aspx.designer.cs
+++ E-Commerce/Views/viewAdmin_AddArt.aspx.designer.cs
@@ -150,12 +150,14 @@ namespace tp_web_equipo_19.Views
         protected global::System.Web.UI.WebControls.PlaceHolder txtImagenUrl_Dinamico;
 
         /// <summary>
-        /// txtPrecio control.
+        /// addImage control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
+      
+
         protected global::System.Web.UI.WebControls.TextBox txtPrecio;
 
         /// <summary>
---
+++ E-Commerce_Models/Mensaje.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace E_Commerce_Models
+{
+    public class Mensaje
+    {
+        public int Id { get; set; }
+        public int ConversacionId { get; set; }
+        public string EmisorId { get; set; } // Id del usuario que envía el mensaje
+        public string ReceptorId { get; set; } // Id del usuario que recibe el mensaje
+        public string Contenido { get; set; }
+        public DateTime FechaEnvio { get; set; }
+
+        //public virtual Conversacion Conversacion { get; set; }
+        //public virtual ApplicationUser Emisor { get; set; }
+        //public virtual ApplicationUser Receptor { get; set; }
+    }
+}
