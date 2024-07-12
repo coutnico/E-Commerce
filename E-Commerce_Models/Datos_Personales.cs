642354a189a8008b08da5578b4bad1ddf3352dbf
coutnico <nicoo_rodrii@hotmail.com>
2024-07-11 23:21:16 -0300

    Add class references DatosPersonales of user

---
+++ E-Commerce_Models/Datos_Personales.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace E_Commerce_Models
+{
+    public class Datos_Personales
+    {
+        public int ID { get; set; }
+        public int ID_Usuario { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Genero { get; set; }
+        public int Telefono { get; set; }
+        public int TelefonoAux { get; set; }
+        public int Documento { get; set; }
+    }
+}
