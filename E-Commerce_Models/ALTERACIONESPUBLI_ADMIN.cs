abd989656f3c3769870c227c4966d1f664a68de1
marianrp09 <marian_rp09@hotmail.com>
2024-07-13 20:34:54 -0300

    New classes (ALTERACIONESPUBLI_ADMIN & ALTERACIONESPUBLI_ADMINNegocio)

---
+++ E-Commerce_Models/ALTERACIONESPUBLI_ADMIN.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace E_Commerce_Models
+{
+    public class ALTERACIONESPUBLI_ADMIN
+    {
+
+        public int Id { get; set; }
+        public int IdPublicacion { get; set; }
+        public DateTime FechaModificacion { get; set; }
+        public int TipoModificacion { get; set; }
+
+
+
+    }
+}
---
+++ E-Commerce_Negocio/ALTERACIONESPUBLI_ADMINNegocio.cs
@@ -0,0 +1,148 @@
+﻿using E_Commerce_Controller;
+using E_Commerce_Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace E_Commerce_Negocio
+{
+    public class ALTERACIONESPUBLI_ADMINNegocio
+    {
+        
+        ConexionDB conexionDB_obj = new ConexionDB();
+
+        SqlDataReader reader = null;
+
+
+        public List<ALTERACIONESPUBLI_ADMIN> ListarAlteraciones()
+
+
+        {
+            List<ALTERACIONESPUBLI_ADMIN> lista = new List<ALTERACIONESPUBLI_ADMIN>();
+
+            try
+            {
+                
+                string query = "Select Id, IdPublicacion,FechaModificacion, TipoModificacion from ALTERACIONESPUBLI_ADMIN";
+
+                reader = conexionDB_obj.LeerDatos(query); ;
+
+
+                while (reader.Read())
+                {
+                    ALTERACIONESPUBLI_ADMIN alteracionespubli_admin = new ALTERACIONESPUBLI_ADMIN();
+
+                    alteracionespubli_admin.Id = Convert.ToInt32(reader["Id"]);
+                    alteracionespubli_admin.IdPublicacion = Convert.ToInt32(reader["IdPublicacion"]);
+                    alteracionespubli_admin.FechaModificacion = Convert.ToDateTime(reader["FechaModificacion"]);
+                    alteracionespubli_admin.Id = Convert.ToInt32(reader["TipoModificacion"]);
+
+                    lista.Add(alteracionespubli_admin);
+
+                }
+
+                return lista;
+
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            finally { conexionDB_obj.CerrarConexion(); }
+
+
+        }
+
+        public void agregarAlteracionAdmin(ALTERACIONESPUBLI_ADMIN alteracionespubli_admin)
+        {
+            ConexionDB conexionDB_Obj = new ConexionDB();
+            try
+            {
+
+                
+                conexionDB_Obj.EjecutarComando("Insert into ALTERACIONESPUBLI_ADMIN (IdPublicacion,FechaModificacion,TipoModificacion) Values (" + alteracionespubli_admin.IdPublicacion + "," + alteracionespubli_admin.FechaModificacion + "," + alteracionespubli_admin.TipoModificacion + ")");
+  
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        public void eliminarAlteracionAdmin(int id_delete)
+        {
+            ConexionDB conexionDB_Obj = new ConexionDB();
+
+            try
+            {
+                // SQL usa ' para el query. y c# com dobles para separar cadenas
+                conexionDB_Obj.EjecutarComando("Delete from ALTERACIONESPUBLI_ADMIN where ID = " + id_delete);
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void modificarAlteracionAdmin(ALTERACIONESPUBLI_ADMIN alteracionespubli_admin_obj, int ID_a_modificar)
+        {
+            ConexionDB conexionDB_Obj = new ConexionDB();
+
+            try
+            {
+               
+                conexionDB_Obj.EjecutarComando("UPDATE ALTERACIONESPUBLI_ADMIN SET IdPublicacion =" + alteracionespubli_admin_obj.IdPublicacion + "," + alteracionespubli_admin_obj.FechaModificacion + "," + alteracionespubli_admin_obj.TipoModificacion +  " WHERE Id = " + ID_a_modificar);
+                
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public ALTERACIONESPUBLI_ADMIN Buscar_AlteracionAdmin_por_ID(int id_buscado)
+
+        {
+            int id_aux = 0;
+            try
+            {
+                string query = "Select Id, IdPublicacion,FechaModificacion, TipoModificacion from ALTERACIONESPUBLI_ADMIN";
+
+                reader = conexionDB_obj.LeerDatos(query);
+
+                ALTERACIONESPUBLI_ADMIN alteracionespubli_admin = new ALTERACIONESPUBLI_ADMIN();
+                while (reader.Read())
+                {
+                    id_aux = Convert.ToInt32(reader["Id"]);
+
+                    if (id_aux == id_buscado)
+                    {
+                        alteracionespubli_admin.Id = id_buscado;//
+                        alteracionespubli_admin.IdPublicacion = Convert.ToInt32(reader["IdPublicacion"]);
+                        alteracionespubli_admin.FechaModificacion = Convert.ToDateTime(reader["FechaModificacion"]);
+                        alteracionespubli_admin.TipoModificacion = Convert.ToInt32(reader["TipoModificacion"]);
+                    }
+                }
+                return alteracionespubli_admin;
+
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            finally { conexionDB_obj.CerrarConexion(); }
+
+
+        }
+
+
+
+    }
+}
